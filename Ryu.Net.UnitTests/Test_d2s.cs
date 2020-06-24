ed9a976ec1c0dd71e914095f0d05845f3c485bea
Ninds <narinder.claire@gmail.com>
2020-06-24 07:02:46 +0100

    Migrate d2s to Span<byte>

--- Ryu.Net.UnitTests/Test_d2s.cs
+++ Ryu.Net.UnitTests/Test_d2s.cs
@@ -12,37 +12,69 @@ namespace RyuDotNet.UnitTests
     unsafe public class Test_d2s
     {
 
-        [Theory]
+        //[Theory]
         [InlineData(5737722933969577e-231)]
         [ClassData(typeof(D2SDataGenerator))]
-        public void Test_d2s_buffered_n(double f)
+        public void Test_d2exp_buffered_n(double f)
         {
             Span<char> readOnlySpan = stackalloc char[100];
             int index = 0;
 
-            try
+            fixed (char* buffer = &readOnlySpan.GetPinnableReference())
             {
-                fixed (char* buffer = &readOnlySpan.GetPinnableReference())
-                {
-                    index = RyuDotNet.Internal.Ryu.d2exp_buffered_n(f, 19, buffer);
-                }
+                index = RyuDotNet.Internal.Ryu.d2exp_buffered_n(f, 19, buffer);
             }
-            catch (AssertException)
-            {
-               File.AppendAllText(@"../.././../FAILS.txt", f.ToString("G17") +  "\r\n");
-               throw;
-            }
-
 
             var strString = new string(readOnlySpan.Slice(0, index));
             var parts = strString.Split('E');
             var expInt = int.Parse(parts[1]);
-            var exp =  expInt.ToString("000");
-            if(expInt>=0)exp = "+" + exp;
+            var exp = expInt.ToString("000");
+            if (expInt >= 0) exp = "+" + exp;
             strString = parts[0] + 'E' + exp;
             Assert.Equal(f.ToString("E19"), strString);
 
         }
+
+
+        [Theory]
+        [ClassData(typeof(D2SSmallDataGenerator))]
+        public void TestWithSmall_d2exp_buffered_n(double f)
+        {
+            Span<char> readOnlySpan = stackalloc char[100];
+            int index = 0;
+
+            fixed (char* buffer = &readOnlySpan.GetPinnableReference())
+            {
+                index = RyuDotNet.Internal.Ryu.d2exp_buffered_n(f, 19, buffer);
+            }
+
+            var strString = new string(readOnlySpan.Slice(0, index));
+            var parts = strString.Split('E');
+            var expInt = int.Parse(parts[1]);
+            var exp = expInt.ToString("000");
+            if (expInt >= 0) exp = "+" + exp;
+            strString = parts[0] + 'E' + exp;
+            Assert.Equal(f.ToString("E19"), strString);
+
+        }
+
+        [Theory]
+        [ClassData(typeof(D2SSmallDataGenerator))]
+        public void TestWithSmall_d2s_buffered_n(double f)
+        {
+            Span<char> readOnlySpan = stackalloc char[100];
+            int index = 0;
+
+            fixed (char* buffer = &readOnlySpan.GetPinnableReference())
+            {
+                index = RyuDotNet.Internal.Ryu.d2s_buffered_n(f,buffer);
+            }
+
+            var strString = new string(readOnlySpan.Slice(0, index));
+            Assert.Equal(f.ToString("E19"), double.Parse(strString).ToString("E19"));
+
+        }
+
 
 
     }
@@ -74,6 +106,25 @@ namespace RyuDotNet.UnitTests
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
     }
+
+    public class D2SSmallDataGenerator : IEnumerable<object[]>
+    {
+        
+        public IEnumerator<object[]> GetEnumerator()
+        {
+          
+                foreach (var v in SmallD2STestSet.TestArray)
+                {
+                    yield return new object[] { v };
+                }
+
+           
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    }
+
 
 }
 
